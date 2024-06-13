278964878870779691503c9d621193d98041326c
thimss1 <ssmithga@gmail.com>
2024-06-13 11:57:12 -0500

    Added test_50%_off_coupon_ss.py

--- test_50%_off_coupon_ss.py
+++ test_50%_off_coupon_ss.py
@@ -41,3 +41,49 @@ class Test50OffCoupon:
         Raises:
             AssertionError: If the final cart total does not reflect a 50% reduction after the coupon is applied.
         """
+        # go th home page
+        home_page = HomePage(self.driver)
+        home_page.go_to_home_page()
+
+        # add item to cart
+        home_page.click_first_add_to_cart_button()
+
+        # make sure the cart is updated before going to cart
+        # because when we click on add to cart and we go to cart, it happens so fast that we
+        # get to the cart page before the cart is updated. So we see that there is '1 item' in the cart on the
+        # top nav bar but the middle of the page says that there is no item in cart
+        header = Header(self.driver)
+        header.wait_until_cart_item_count(1)
+
+        # go to cart
+        cart_page = CartPage(self.driver)
+        cart_page.go_to_cart_page()
+
+        # before applying the coupon get the total amount
+        total_price_before_coupon = cart_page.get_cart_total()
+
+        # apply a 50% off coupon
+        # first get the coupon from config
+        coupon_50_off = MainConfigs.get_coupon_code("50 OFF")
+        cart_page.apply_coupon(coupon_50_off)
+
+        # before getting the cart total, verify the page has updated
+        cart_page.verify_displayed_success_messafe('Coupon code applied successfully.')
+        time.sleep(1)
+
+        # after applying the coupon get the total amount
+        total_price_after_coupon = cart_page.get_cart_total()
+
+        # the coupon is supposed to give 50% off. So calculate what should be 50% off the original price
+        expected_new_total = .5 * total_price_before_coupon
+
+        # if expected_new_total !=total_price_after_coupon:
+        #   raise Exceptional(f"After applying 50% off coupon, the total cart price did not update by 50%. Coupon code = {coupon_50_off}")
+
+        assert expected_new_total == total_price_after_coupon, f"After applying 50% off coupon, "\
+                        "the total cart price did not update by 50%. Coupon code = '{coupon_50_off}' "\
+                        f"Expected total: {expected_new_total}, Actual page total: {total_price_after_coupon}"
+
+
+
+
